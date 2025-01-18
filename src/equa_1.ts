7bfd239a6424299f7737a96871ae6fb3110fc36c
Luis Armando <mariana@MacBook-Air-de-Luis-Armando.local>
2025-01-18 14:18:56 -0600

    equa_1

--- src/equa_1.ts
+++ src/equa_1.ts
@@ -2,4 +2,11 @@ import { create, all } from "https://esm.sh/mathjs?bundle";
 
 const math = create(all, {});
 
-console.log(math)
+console.log(getNonNestedFunctions("(n * (n + 1) * (2n + 1) * (3n^4 + 6n^3 - n^2 - n + 1)) / 42"))
+
+function getNonNestedFunctions(input: string): string[] {
+    const matches = input.match(/\([ \+\-\*\/\^0-9a-z]+\)/gi);
+
+    return matches ? matches.map(m => m.trim()) : [];
+}
+
